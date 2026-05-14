ea16312e7abef118b67a229e2662a0e58ea24c76
Jewel <m.76jewel@gmail.com>
2026-05-14 18:44:13 +0600

    Updated

--- server/index.js
+++ server/index.js
@@ -87,7 +87,14 @@ const app = express()
 // const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT || 5000;
 
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        process.env.FRONTEND_URL,
+    ].filter(Boolean),
+    credentials: true,
+}));
 app.use(express.json())
 
 const client = new MongoClient(uri, {
